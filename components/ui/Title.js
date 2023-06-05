71786c3197217cd48a2baa3f7fd60198c38b1577
Aidan Kirvan <67266954+teraglin@users.noreply.github.com>
2023-06-05 16:51:35 +1000

    Task: random number generation

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,5 +1,5 @@
 import { Text, StyleSheet } from "react-native";
-import Colors from "../constants/colors";
+import Colors from "../../constants/colors";
 
 function Title(props) {
   return <Text style={styles.title}>{props.children}</Text>;
@@ -11,10 +11,10 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: "bold",
-    color: Colors.accent500,
+    color: 'white',
     textAlign: "center",
     borderWidth: 2,
-    borderColor: Colors.accent500,
+    borderColor: 'white',
     padding: 12,
   },
 });
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,11 +1,26 @@
 import { View, Text, StyleSheet } from "react-native";
-import Title from "../components/Title";
+import Title from "../components/ui/Title";
+import { useState } from "react";
+import NumberContainer from "../components/game/NumberContainer";
 
-function GameScreen() {
+function generateRandomBetween(min, max, exclude) {
+  const rndNumber = Math.floor(Math.random() * (max - min)) + min;
+
+  if (rndNumber === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNumber;
+  }
+}
+
+function GameScreen({ userNumber }) {
+  const initialGuess = generateRandomBetween(1, 100, userNumber);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+
   return (
     <View style={styles.screen}>
       <Title>Opponent's Guess</Title>
-      {/* GUESS */}
+      <NumberContainer>{currentGuess}</NumberContainer>
       <View>
         <Text>Higher or Lower?</Text>
         {/* + - */}
